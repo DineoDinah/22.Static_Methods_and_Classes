38b1c1ed8e44eb7e657397d1066c57e49923c161
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-09 22:56:53 +0200

    Add project files.

---
+++ 22.Static_Methods_and_Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // static is the about class itself, no need to create objectsto get to the class itself.
+
+            Console.WriteLine(Math.Sqrt(64));
+            //UseFulL tools = new UseFullTools(); if u maje the class static as well, u will only access it with direct methods,
+            //no more objects, just like u cant make an object of the Math class
+            UseFullTools.sayHi("Dineo"); 
+            //
+
+            //freeze
+            Console.ReadLine();
+
+        }
+        static internal class UseFullTools
+            //this make this class only accesible through methods not through objects
+        {
+            public static void sayHi(String name)
+            {
+
+                Console.WriteLine("Hi" + name);
+             }
+        }
+    }
+}
